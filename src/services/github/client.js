c8a9f577e73ce0f343fe692562bc86f2b01f8972
Carlos Pure Serrano <carlospures@gmail.com>
2024-01-07 20:53:54 -0500

    added oauth app

--- src/services/github/client.js
+++ src/services/github/client.js
@@ -1,7 +1,19 @@
-const { Octokit, App } = require("octokit");
+const { Octokit } = require("octokit");
+const { createTokenAuth } = require('@octokit/auth-token');
 
+const client_secret = 'e406eef7644b2bd0457647f96f528c96ff0b0ef5'
+const client_id ='2b609d658f07929caf6a'
+
+const auth = createTokenAuth(client_id,client_secret);
+let token = "";
+auth()
+  .then(authData => {
+    token= authData.token
+  })
+  .catch(error => {
+    console.error(error.message)
+  });
 const octokit = new Octokit({ 
-  auth: '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'
-});
-
+  auth: token
+})
 module.exports = octokit
